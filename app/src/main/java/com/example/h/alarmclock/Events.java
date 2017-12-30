874514ec8c8d382158e799e16843985a67bb1a42
Harshal Lele <frost17121997@gmail.com>
2017-12-30 18:44:17 +0530

    moved all EventBus events to a separate class

---
+++ app/src/main/java/com/example/h/alarmclock/Events.java
@@ -0,0 +1,16 @@
+package com.example.h.alarmclock;
+
+/**
+ * Created by h on 12/30/17.
+ */
+
+public class Events {
+
+    // Event for when an alarm is swiped away.
+    public static class DeleteAlarmEvent { /* Additional fields if needed */ }
+
+    //For when an alarm is added
+    public static class AlarmAddedEvent{}
+
+
+}
