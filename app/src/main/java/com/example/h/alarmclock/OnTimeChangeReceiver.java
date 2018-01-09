66f39ca15b492fd3573aad7803492269a92bcbf7
Harshal Lele <frost17121997@gmail.com>
2018-01-09 20:48:24 +0530

    added receivers for boot,change in time, and timezone

---
+++ app/src/main/java/com/example/h/alarmclock/OnTimeChangeReceiver.java
@@ -0,0 +1,26 @@
+package com.example.h.alarmclock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by h on 1/9/18.
+ */
+
+public class OnTimeChangeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // To get set of all registered ids on boot.
+        Set<String> set = SimpleAlarmManager.getAllRegistrationIds(context);
+        for (Iterator<String> it = set.iterator(); it.hasNext(); ) {
+            int id = Integer.parseInt(it.next());
+            //to initialize with registreation id
+            SimpleAlarmManager.initWithId(context, id).start();
+        }
+    }
+}
